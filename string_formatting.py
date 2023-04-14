cb35377ab1b0187d1607a51dc57ec00cdf4b2ff2
kunwarp29 <kunwarpratap353@gmail.com>
2023-04-14 16:15:26 +0530

    added few lines of code

--- string_formatting.py
+++ string_formatting.py
@@ -7,10 +7,26 @@ its not a good way to write strings so string fomatting is required.
 '''
 
 formatted_sentence = 'My name is {} and I am {} years old'.format(person['name'],person['Age'])
-print("formatted_string",formatted_sentence)
+#other way to write the string is
+formatted_sentence_other = 'My name is {0[name]} and I am {1[Age]} years old'.format(person,person)
+
+print("formatted_string: ",formatted_sentence)
+print("formatted_string_other: ",formatted_sentence_other)
+
 #one more example
 
 tag = 'h1'
 text = 'this is a headline'
-sentence = '<{0}> {1} </{0}>'.format(tag,text)
+sentence = '<{0}> {1} </{0}>'.format(tag,text)  #index pass in the placeholder
 print(sentence)
+'''-------------------------------------------------------------------------------------------------------------------------------'''
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person('Ramesh','33')
+
+sentence = 'My name is {0.name} and I am {0.age} years old.'.format(p1)
+print("class_string: ",sentence)
