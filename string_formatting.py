597f06ed3ca085c9204ca04de0f23420fcb3a72a
kunwarp29 <kunwarpratap353@gmail.com>
2023-04-14 16:41:00 +0530

    added range, class sttring formatting

--- string_formatting.py
+++ string_formatting.py
@@ -9,9 +9,14 @@ its not a good way to write strings so string fomatting is required.
 formatted_sentence = 'My name is {} and I am {} years old'.format(person['name'],person['Age'])
 #other way to write the string is
 formatted_sentence_other = 'My name is {0[name]} and I am {1[Age]} years old'.format(person,person)
+#one more way of doing so
+formatted_sentence_other2 = 'My name is {name} and I am {Age} years old'.format(**person)
+
 
 print("formatted_string: ",formatted_sentence)
 print("formatted_string_other: ",formatted_sentence_other)
+print("formatted_string_other2: ",formatted_sentence_other2)
+
 
 #one more example
 
@@ -30,3 +35,12 @@ p1 = Person('Ramesh','33')
 
 sentence = 'My name is {0.name} and I am {0.age} years old.'.format(p1)
 print("class_string: ",sentence)
+
+
+for i in range(1,11):
+    sentence = 'the value is {}'.format(i)
+    print(sentence)
+
+for i in range(1,11):
+    sentence = 'the value is {:03}'.format(i)       #padding of zero and 3 digit places.
+    print(sentence)    
